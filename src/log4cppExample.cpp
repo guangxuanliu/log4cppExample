ccc12cf40b3dffe8778b5156127810ffba740088
guangxuanliu <guangxuanliu@gmail.com>
2020-03-11 16:51:08 +0800

    add multithread

--- src/log4cppExample.cpp
+++ src/log4cppExample.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <sstream>
 #include <log4cpp/Category.hh>
 #include <log4cpp/FileAppender.hh>
 #include <log4cpp/BasicLayout.hh>
@@ -56,30 +57,12 @@ void manualWrite() {
 	log4cpp::Category::shutdown();
 }
 
-void configWrite() {
-	// 1 读取解析配置文件
-	// 读取出错, 完全可以忽略，可以定义一个缺省策略或者使用系统缺省策略
-	// BasicLayout输出所有优先级日志到ConsoleAppender
-	try {
-		log4cpp::PropertyConfigurator::configure("./log4cpp.conf");
-	} catch (log4cpp::ConfigureFailure &f) {
-		std::cout << "Configure Problem " << f.what() << std::endl;
-		return;
-	}
-
-	// 2 实例化category对象
-	// 这些对象即使配置文件没有定义也可以使用，不过其属性继承其父category
-	// 通常使用引用可能不太方便，可以使用指针，以后做指针使用
-	// log4cpp::Category* root = &log4cpp::Category::getRoot();
-
-	log4cpp::Category &sub1 = log4cpp::Category::getInstance(
-			std::string("sub1"));
+void configWrite(log4cpp::Category &sub1) {
 
 	// 3 正常使用这些category对象进行日志处理。
 	// sub1 has appender A1.
 
-	for(int i = 0;i < 5000;i ++)
-	{
+	for (int i = 0; i < 5000; i++) {
 		sub1.info(to_string(i) + " This is some info");
 	}
 
@@ -92,7 +75,38 @@ int main() {
 //	manualWrite();
 
 	// test2
-	configWrite();
+
+	// 1 读取解析配置文件
+	// 读取出错, 完全可以忽略，可以定义一个缺省策略或者使用系统缺省策略
+	// BasicLayout输出所有优先级日志到ConsoleAppender
+	try {
+		log4cpp::PropertyConfigurator::configure("./log4cpp.conf");
+	} catch (log4cpp::ConfigureFailure &f) {
+		std::cout << "Configure Problem " << f.what() << std::endl;
+		return -1;
+	}
+
+	// 2 实例化category对象
+	// 这些对象即使配置文件没有定义也可以使用，不过其属性继承其父category
+	// 通常使用引用可能不太方便，可以使用指针，以后做指针使用
+	// log4cpp::Category* root = &log4cpp::Category::getRoot();
+
+	log4cpp::Category &sub1 = log4cpp::Category::getInstance(
+			std::string("sub1"));
+
+	const int threadNum = 10;
+	thread mythread[threadNum];
+
+	for(int i = 0;i < threadNum;++i)
+	{
+		mythread[i] = thread(configWrite, std::ref(sub1));
+	}
+
+	for(int i = 0;i < threadNum;++i)
+	{
+		mythread[i].join();
+	}
+
 
 	return 0;
 }
